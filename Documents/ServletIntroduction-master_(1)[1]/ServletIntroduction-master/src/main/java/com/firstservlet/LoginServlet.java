e032066e3d2f5a57c70923d280ac84bb33ee85d0
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-02-28 15:20:02 +0530

    Create a simple Login with predefined user

---
+++ Documents/ServletIntroduction-master_(1)[1]/ServletIntroduction-master/src/main/java/com/firstservlet/LoginServlet.java
@@ -0,0 +1,38 @@
+package com.firstservlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.regex.Pattern;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = {
+                @WebInitParam(name = "user", value = "Kirti"),
+                @WebInitParam(name = "password", value = "Kirti@44")
+        }
+)
+
+public class LoginServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
+
+
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+
+
+    }
+
+}
